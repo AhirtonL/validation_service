d42e6f55616cced56700a17ce02ab21ac48dd81f
Gustavo Vargas Castilhos <gustavo.vargas.castilhos@everis.com>
2018-01-28 16:03:45 -0200

    test2

--- validation_gateway.js
+++ validation_gateway.js
@@ -11,13 +11,13 @@ const COMMAND = "python validation_driver.py "
 
 router.post('/', function(req, res){
     res.send('OKKKKKK')
-	var data = req.body
-	var doc = JSON.stringify(data.doc)
-	var base = JSON.stringify(data.base)
-	var regras = JSON.stringify(data.regras)
-	var args = doc + ' ' + base + ' ' + regras
-	var out = proc.execSync(COMMAND + args).toString('utf8')
-	res.send(out)
+	//var data = req.body
+	//var doc = JSON.stringify(data.doc)
+	//var base = JSON.stringify(data.base)
+	//var regras = JSON.stringify(data.regras)
+	//var args = doc + ' ' + base + ' ' + regras
+	//var out = proc.execSync(COMMAND + args).toString('utf8')
+	//res.send(out)
 });
 
 app.use(bodyParser.urlencoded({extended: true}))
